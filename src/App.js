d402575c8379ff438603c648184db84fcaa988e7
Robin Wieruch <wrobin@gmx.net>
2019-04-05 21:55:13 +0200

    only render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const todos = [
+  {
+    id: 'a',
+    task: 'Learn React',
+    completed: true,
+    link: 'https://www.robinwieruch.de/the-road-to-learn-react/',
+  },
+  {
+    id: 'b',
+    task: 'Learn Firebase',
+    completed: true,
+    link:
+      'https://www.robinwieruch.de/the-road-to-react-with-firebase-book/',
+  },
+  {
+    id: 'c',
+    task: 'Learn GraphQL',
+    completed: false,
+    link: 'https://www.robinwieruch.de/the-road-to-graphql-book/',
+  },
+];
+
+const App = () => (
+  <div>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => {}}
+            />
+            {todo.task}
+          </label>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default App;
