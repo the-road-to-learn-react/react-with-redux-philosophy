907b387b3315e760b8b76d0e08d045e65f0bd54b
Robin Wieruch <wrobin@gmx.net>
2019-05-13 17:02:15 +0200

    naming

--- src/App.js
+++ src/App.js
@@ -68,17 +68,17 @@ const todoReducer = (state, action) => {
   }
 };
 
-const useCombinedReducer = useReducers => {
+const useCombinedReducer = combinedReducers => {
   // Global State
-  const state = Object.keys(useReducers).reduce(
-    (acc, key) => ({ ...acc, [key]: useReducers[key][0] }),
+  const state = Object.keys(combinedReducers).reduce(
+    (acc, key) => ({ ...acc, [key]: combinedReducers[key][0] }),
     {}
   );
 
   // Global Dispatch Function
   const dispatch = action =>
-    Object.keys(useReducers)
-      .map(key => useReducers[key][1])
+    Object.keys(combinedReducers)
+      .map(key => combinedReducers[key][1])
       .forEach(fn => fn(action));
 
   return [state, dispatch];
